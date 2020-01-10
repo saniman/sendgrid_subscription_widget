2d2d7f4b4836e1b618417d1a027566293437d627
saniman <saniman32@gmail.com>
2020-01-11 03:23:50 +0900

    amp form action-xhr test

--- server/router.js
+++ server/router.js
@@ -2,14 +2,14 @@ const path = require('path');
 const ContactList = require('./controllers/contact_list_controller');
 
 module.exports = function(app) {
-	app.get('/', function(req, res) { res.sendFile(path.join(__dirname, '/static/index.html')) });
-	app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
-	app.post('/confirmEmail', ContactList.sendConfirmation, function(req,res){
+	app.get('/', function(req, res) { 
 		res.setHeader('Content-type', 'application/json');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*.ampproject.org');
     res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'http://' + req.headers.host);
     res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
-	});
+	 });
+	app.get('/success', function(req, res) { res.sendFile(path.join(__dirname, '/static/success.html')) });
+	app.post('/confirmEmail', ContactList.sendConfirmation);
 	app.post('/signup', ContactList.addUser);
 }
